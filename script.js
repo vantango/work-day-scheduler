ec98a3df7c98a0be8988d434e5eb7e44b0c7f033
Aleks Kimball-Moreno <alekskimball@gmail.com>
2020-12-17 16:38:20 -0800

    removed object array. created function that saves text input into the local storage on the save button click. added function that also pulls back from local storage and applies styling to each row dependent on past, present, and futuretime.

--- script.js
+++ script.js
@@ -4,55 +4,54 @@ $(".date").text(currentDay);
 
 // Sets current time
 var currentTime = dayjs().hour();
+var timeFormat = dayjs().format("h A").toString()
 
-var workSchedule = [
-    {
-        time: "9AM",
-        event: ""
-    },
-    {
-        time: "10AM",
-        event: ""
-    },
-    {
-        time: "11AM",
-        event: ""
-    },
-    {
-        time: "12PM",
-        event: ""
-    },
-    {
-        time: "1PM",
-        event: ""
-    },
-    {
-        time: "2PM",
-        event: ""
-    },
-    {
-        time: "3PM",
-        event: ""
-    },
-    {
-        time: "4PM",
-        event: ""
-    },
-    {
-        time: "5PM",
-        event: ""
+
+// var workSchedule = [
+//     { time: "9AM", item: "" },
+//     { time: "10AM", item: "" },
+//     { time: "11AM", item: "" },
+//     { time: "12PM", item: "" },
+//     { time: "1PM", item: "" },
+//     { time: "2PM", item: "" },
+//     { time: "3PM", item: "" },
+//     { time: "4PM", item: "" },
+//     { time: "5PM", item: "" }
+// ]
+
+// 
+$(".row").each(function () {
+    var hour = $(this).find(".time-block").text()
+    console.log("hour", hour)
+    var textInput = localStorage.getItem(hour);
+    // console.log(textInput)
+    $(this).find(".textarea").val(textInput)
+
+    // Setting color code to current hour
+    hourNum = parseInt($(this).find(".time-block").text().split(" ")[0])
+    if (hourNum < currentTime) {
+        $(this).addClass("past")
+        console.log(hour)
+    } else if (hourNum === currentTime) {
+        $(this).removeClass("past")
+        $(this).addClass("present")
+    } else {
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
     }
-]
 
-// Set local storage
-localStorage.setItem("schedule", JSON.stringify(workSchedule));
+    // test code
+    // if (hour === timeFormat) {
+    //     console.log("test message")
+    //     $(this).addClass("present")
+    // }
+})
 
-// Pull local storage
-var textInput = JSON.parse(localStorage.getItem("schedule"));
-console.log(textInput)
-if (textInput) {
-    schedule = textInput;
-};
-
-
+// Sets local storage on button click
+$(".saveBtn").on("click", function () {
+    var time = $(this).siblings(".time-block").text()
+    var description = $(this).siblings(".textarea").val()
+    localStorage.setItem(time, description);
+})
 
